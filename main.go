5ef874fd638435d1df2dcc45547ddc83dbaf6332
Nick Robinson <nr33@msstate.edu>
2015-09-12 10:32:06 -0400

    Updating main to accept flags for DB info

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"./checker"
 	"./notifier"
 )
@@ -12,9 +13,15 @@ func main() {
 	c := make(chan int)
 	fmt.Println("Hello World")
 
-	checkClient.NewChecker("./redfactor.db", "192.168.1.206", 8086, "redfactor", "cpu")
+	var dbFile = flag.String("db", "./redfactor.db", "SQLite Database File")
+	var hostname = flag.String("host", "127.0.0.1", "InfluxDB Hostname")
+	var port = flag.Int("port", 8086, "InfluxDB Port")
+	var database = flag.String("database", "redfactor", "InfluxDB Database Name")
+	flag.Parse()
+
+	checkClient.NewChecker(*dbFile, *hostname, *port, *database, "cpu")
 
 	go checkClient.StartChecker(c)
-	go notifier.RunNotifier(c)
+	go notifier.StartNotifier(c)
 	select {}
 }
